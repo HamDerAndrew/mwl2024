60e96c24e1c51022106346af4040c2edd9f7b91d
Andrew <andrewsondergaard@gmail.com>
2024-01-25 22:44:53 +0100

    Style: services content, iconify, minor stylechanges

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+const { addDynamicIconSelectors } = require('@iconify/tailwind');
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -5,8 +7,15 @@ export default {
 		extend: {
 			boxShadow: {
 				'3xl': '0 2px 4px 0 rgba(0, 0, 0, 0.25)',
+			},
+			rotate: {
+				'left': '-6deg',
+				'right': '6deg'
 			}
 		},
 	},
-	plugins: [],
+	plugins: [ 
+		// Iconify plugin
+		addDynamicIconSelectors(),
+	],
 }
