d15f352f47085cbc280ba61f6ec569436c1fc9ae
Andrew <andrewsondergaard@gmail.com>
2024-06-06 00:24:23 +0200

    Feature: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://mwl.nu',
+  integrations: [tailwind(), sitemap()],
   i18n: {
     defaultLocale: "en",
-    locales: ["dk", "en"],
+    locales: ["dk", "en"]
   }
 });
